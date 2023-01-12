5d5fc4a8b160e0fed87438471cb71bb9c0fad243
Kaveesh Charuka <63943539+kaveesh680@users.noreply.github.com>
2023-01-12 20:27:14 +0530

    Update Timer_test.v

--- Timer_test.v
+++ Timer_test.v
@@ -1,27 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   19:12:16 06/12/2019
-// Design Name:   Timer
-// Module Name:   C:/Users/Aruna/Documents/ISE/TrafficLightController/Timer_test.v
-// Project Name:  TrafficLightController
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: Timer
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module Timer_test;
 
 	// Inputs
