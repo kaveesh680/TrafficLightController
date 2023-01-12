537855b6331d37e2c6011f8e32d0028eddbcec0f
Kaveesh Charuka <63943539+kaveesh680@users.noreply.github.com>
2023-01-12 20:25:47 +0530

    Update Synchronizer.v

--- Synchronizer.v
+++ Synchronizer.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    17:35:58 06/13/2019 
-// Design Name: 
-// Module Name:    Synchronizer 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module Synchronizer(
     input Reset,
     input Sensor,
