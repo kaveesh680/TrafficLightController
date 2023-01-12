84607a9cd46571f572c3ad862cb9b7863d5c4fe4
Kaveesh Charuka <63943539+kaveesh680@users.noreply.github.com>
2023-01-12 20:27:31 +0530

    Update TrafficControllerMain.v

--- TrafficControllerMain.v
+++ TrafficControllerMain.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    18:10:53 06/13/2019 
-// Design Name: 
-// Module Name:    TrafficControllerMain 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module TrafficControllerMain(
     input Reset,
     input Sensor,
