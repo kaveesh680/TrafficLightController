33bd9443888e4914192a5ba4a4ce4ef55eae1a85
Kaveesh Charuka <63943539+kaveesh680@users.noreply.github.com>
2023-01-12 20:28:31 +0530

    Update WalkRegister_test.v

--- WalkRegister_test.v
+++ WalkRegister_test.v
@@ -1,27 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   17:12:45 06/13/2019
-// Design Name:   WalkRegister
-// Module Name:   C:/Users/Aruna/Documents/ISE/TrafficLightController/WalkRegister_test.v
-// Project Name:  TrafficLightController
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: WalkRegister
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module WalkRegister_test;
 
 	// Inputs
