0a9f4880244c760d49c50b1bbaa9697b5578694e
Kaveesh Charuka <63943539+kaveesh680@users.noreply.github.com>
2023-01-12 20:28:16 +0530

    Update WalkRegister.v

--- WalkRegister.v
+++ WalkRegister.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    16:59:48 06/13/2019 
-// Design Name: 
-// Module Name:    WalkRegister 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module WalkRegister(
     input WR_Sync,
     input WR_Reset,
