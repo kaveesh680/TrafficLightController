29ede121eacfb79c978c5c2d8a6ad26dcaff24fd
Kaveesh Charuka <63943539+kaveesh680@users.noreply.github.com>
2023-01-12 20:26:43 +0530

    Update TimeParameter_test.v

--- TimeParameter_test.v
+++ TimeParameter_test.v
@@ -1,27 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   15:02:27 06/13/2019
-// Design Name:   TimeParameter
-// Module Name:   C:/Users/Aruna/Documents/ISE/TrafficLightController/TimeParameter_test.v
-// Project Name:  TrafficLightController
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: TimeParameter
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module TimeParameter_test;
 
 	// Inputs
