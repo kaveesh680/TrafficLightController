faf2b8cf29fb1ec5ed838dde29f5a4f98dd17e41
Kaveesh Charuka <63943539+kaveesh680@users.noreply.github.com>
2023-01-12 20:26:05 +0530

    Update Synchronizer_test.v

--- Synchronizer_test.v
+++ Synchronizer_test.v
@@ -1,27 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   17:53:54 06/13/2019
-// Design Name:   Synchronizer
-// Module Name:   C:/Users/Aruna/Documents/ISE/TrafficLightController/Synchronizer_test.v
-// Project Name:  TrafficLightController
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: Synchronizer
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module Synchronizer_test;
 
 	// Inputs
