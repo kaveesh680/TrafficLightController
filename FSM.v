809090a88321221fb7fd0c41141f5e80538c4f73
Kaveesh Charuka <63943539+kaveesh680@users.noreply.github.com>
2023-01-12 20:24:57 +0530

    Update FSM.v

--- FSM.v
+++ FSM.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    20:39:44 06/11/2019 
-// Design Name: 
-// Module Name:    FSM 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 // LED sequence  : [Rm,Ym,Gm,Rs,Ys,Gs,Walk]
 module FSM(
     input Sensor_Sync,
